f1f2e06274f54bbb957a17d722d8a3325ebaeb0a
AviralPatel0526 <aviralpatel2603@gmail.com>
2023-10-30 20:13:02 +0530

    Auth form done

--- src/pages/AuthForm.jsx
+++ src/pages/AuthForm.jsx
@@ -9,36 +9,37 @@ function AuthForm() {
   };
 
   return (
+   <div className='flex h-[100vh] w-[100%] items-center justify-center'>
     <div className={`container ${isSignUp ? 'right-panel-active' : ''}`}>
       <div className="form-container sign-up-container">
         {isSignUp && (
-          <form action="#">
-            <h1>Create Account</h1>
+          <form className="sign" action="#">
+            <h1 className="font-bold m-0">Create Account</h1>
             <div className="social-container">
               <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
               <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
               <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
             </div>
-            <span>or use your email for registration</span>
-            <input type="text" placeholder="Name" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
+            <span className="text-[12px]">or use your email for registration</span>
+            <input  className="signInput" type="text" placeholder="Name" />
+            <input  className="signInput" type="email" placeholder="Email" />
+            <input  className="signInput" type="password" placeholder="Password" />
             <button>Sign Up</button>
           </form>
         )}
       </div>
       <div className="form-container sign-in-container">
         {!isSignUp && (
-          <form action="#">
-            <h1>Sign in</h1>
+          <form className="sign" action="#">
+            <h1 className="font-bold m-0">Sign in</h1>
             <div className="social-container">
               <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
               <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
               <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
             </div>
-            <span>or use your account</span>
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
+            <span className="text-[12px]">or use your account</span>
+            < input className="signInput"  type="email" placeholder="Email" />
+            <input className="signInput"  type="password" placeholder="Password" />
             <a href="#">Forgot your password?</a>
             <button>Sign In</button>
           </form>
@@ -47,21 +48,22 @@ function AuthForm() {
       <div className="overlay-container">
         <div className="overlay">
           <div className={`overlay-panel overlay-left ${isSignUp ? 'right-panel-active' : ''}`}>
-            <h1>Welcome Back!</h1>
-            <p>To keep connected with us, please login with your personal info</p>
-            <button className="ghost" id="signIn" onClick={toggleMode}>
+            <h1 className="font-bold  p-2">Welcome Back!</h1>
+            <p className="text-[14px] pb-2">To keep connected with us, please login with your personal info</p>
+            <button className="ghost p-2" id="signIn" onClick={toggleMode}>
               Sign In
             </button>
           </div>
           <div className={`overlay-panel overlay-right ${isSignUp ? '' : 'right-panel-active'}`}>
-            <h1>Hello, Friend!</h1>
-            <p>Enter your personal details and start your journey with us</p>
-            <button className="ghost" id="signUp" onClick={toggleMode}>
+            <h1 className="font-bold  p-2">Hello, Friend!</h1>
+            <p className="text-[14px] pb-2">Enter your personal details and start your journey with us</p>
+            <button className="ghost p-2" id="signUp" onClick={toggleMode}>
               Sign Up
             </button>
           </div>
         </div>
       </div>
+    </div>
     </div>
   );
 }
