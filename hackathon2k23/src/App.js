b80ba827d3114a102ff3f440456e0588a236b7b2
Deewakar <deewakarrao59@gmail.com>
2023-10-30 08:57:16 +0530

    initialization and panolens done

---
+++ hackathon2k23/src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import PanoramaViewer from './components/PanoramaViewer';
+
+function App() {
+  return (
+    <>
+    {/* <PanoramaViewer/> */}
+    </>
+  );
+}
+
+export default App;
