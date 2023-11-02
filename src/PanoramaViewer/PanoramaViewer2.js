e596d40fc2947aa25c8786f24dd100cd76018aad
Deewakar <deewakarrao59@gmail.com>
2023-11-02 10:45:01 +0530

    added new Tenant Cards

---
+++ src/PanoramaViewer/PanoramaViewer2.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useRef } from 'react';
+import * as PANOLENS from 'panolens';
+import * as THREE from 'three';
+import image from '../assets/image1.jpeg';
+
+const PanoramaViewer2 = (prop) => {
+  const viewerRef = useRef(null);
+
+  useEffect(() => {
+    const panoramaImage = new PANOLENS.ImagePanorama(image);
+    const viewer = new PANOLENS.Viewer({
+      container: viewerRef.current,
+      autoRotate: true,
+      autoRotateSpeed: 0.3,
+      controlBar: false,
+    });
+    viewer.add(panoramaImage);
+  }, []);
+
+  return (
+    <div className='flex h-[100vh] items-center bg-black'>
+      <div ref={viewerRef} className="flex h-[100%] fixed w-[100%]"></div>
+    </div>
+    
+  );
+};
+
+export default PanoramaViewer2;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,7 +28,7 @@ function Navbar() {
       <div><img src="https://cdn.builder.io/api/v1/image/assets/TEMP/fc57725c-8ab0-43ad-a8c1-24eb43956f47?apiKey=0e2d2b3fdaa848b2ac6dc9f62f11a5f8"/></div>
       <div className='md:flex h-[10vh] justify-center items-center gap-14 hidden'>
         <div className='hover:scale-110'><a href="/">Home</a></div>
-        <div className='hover:scale-110'><a href="/Tenants">For Tenants</a></div>
+        <div className='hover:scale-110'><a href="/TenantsList">For Tenants</a></div>
         <div className='hover:scale-110'><a href="/Owners">For Owners</a></div>
         <div id="signup" className='border-[1px] border-black p-2 rounded-lg hover:scale-110'><button><a href="/SignUp">SignUp</a></button></div>
         {/* <div id="logout" className='border-[1px] border-black p-2 rounded-lg hover:scale-110 hidden'><button>Log Out</button></div>
@@ -41,11 +41,10 @@ function Navbar() {
     
      <div className='flex justify-center items-center h-[90vh] w-[100vw] md:hidden'>
      <div className='flex flex-col gap-8 text-center'>
-     <div><a href=""  className='font-semibold hover:scale-110'>Home</a></div>
-     <div><a href="" className='font-semibold hover:scale-110'>For Tenants</a></div>
-     <div><a href="" className='font-semibold hover:scale-110'>For Owners</a></div>
-     <div><a href="" className='font-semibold hover:scale-110'>Login</a></div>
-     <div><button><a href="" className='border-[1px] border-black p-2 rounded-lg font-bold'>SignUp</a></button></div>
+     <div><a href="/"  className='font-semibold hover:scale-110'>Home</a></div>
+     <div><a href="/TenantsList" className='font-semibold hover:scale-110'>For Tenants</a></div>
+     <div><a href="/Owners" className='font-semibold hover:scale-110'>For Owners</a></div>
+     <div><button><a href="/SignUP" className='border-[1px] border-black p-2 rounded-lg font-bold'>SignUp</a></button></div>
      </div>
    
    </div>
